2605b608f5fa302350be5450777270e4207594f8
iBog <iBog@users.noreply.github.com>
2023-07-17 14:45:39 +0300

    added mocked methods on Android

---
+++ android/src/main/kotlin/com/example/app/ext/link/ext_link_account_plugin/ExtLinkAccountPlugin.kt
@@ -0,0 +1,41 @@
+package com.example.app.ext.link.ext_link_account_plugin
+
+import androidx.annotation.NonNull
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+
+/** ExtLinkAccountPlugin */
+class ExtLinkAccountPlugin: FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "ext_link_account_plugin")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else if (call.method == "canOpenExternalLinkAccount") {
+      return result.success(false);
+    } else if (call.method == "canMakePayments") {
+      return result.success(false);
+    } else if (call.method == "externalLinkOpen") {
+      return result.success(null);
+    } else {
+      result.notImplemented()
+    }
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+}
---
+++ example/android/app/src/main/kotlin/com/example/app/ext/link/ext_link_account_plugin_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app.ext.link.ext_link_account_plugin_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
